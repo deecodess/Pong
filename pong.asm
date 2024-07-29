cecb904495750096a82bb949e23941e9b17ab490
Deepanshi Sharma <deepanshisharma05@gmail.com>
2024-07-30 01:40:48 +0530

    We have the ball

--- pong.asm
+++ pong.asm
@@ -5,11 +5,21 @@ STACK ENDS
 DATA SEGMENT PARA 'DATA'
     BALL_X DW 0Ah ; x pos
     BALL_Y DW 0Ah ; y pos
+    BALL_SIZE DW 04h ;ball size
 DATA ENDS
 
 CODE SEGMENT PARA 'CODE'
  
     MAIN PROC FAR
+    ASSUME CS:CODE,DS:DATA,SS:STACK
+    PUSH DS
+    SUB AX,AX
+    PUSH AX
+    MOV AX,DATA
+    MOV DS,AX
+    POP AX
+    POP AX
+
         MOV AH,00h ;seting video mode
         MOV AL,13h ;choosing the video mode
         INT 10h
@@ -19,15 +29,39 @@ CODE SEGMENT PARA 'CODE'
         MOV BL,00h ;black
         INT 10h
 
-        MOV AH,0Ch 
-        MOV AL,0Fh ; white as pixel color
-        MOV BH,00h ; setting up the page number
-        MOV CX,BALL_X ;x column
-        MOV DX,BALL_Y ;Y row
-        INT 10h
+        CALL DRAW_BALL
 
         RET
     MAIN ENDP
+
+    DRAW_BALL PROC NEAR
+        
+        MOV CX,BALL_X ;initial x column
+        MOV DX,BALL_Y ;initial Y row
+
+        DRAW_BALL_HORIZONTAL:
+            MOV AH,0Ch 
+            MOV AL,0Fh ; white as pixel color
+            MOV BH,00h ; setting up the page number
+            INT 10h
+            INC CX
+
+            MOV AX,CX
+            SUB AX,BALL_X
+            CMP AX, BALL_SIZE
+            JNG DRAW_BALL_HORIZONTAL
+
+            MOV CX,BALL_X
+            INC DX
+
+            MOV AX,DX
+            SUB AX,BALL_Y
+            CMP AX,BALL_SIZE
+            JNG DRAW_BALL_HORIZONTAL
+
+
+        RET
+    DRAW_BALL ENDP
 CODE ENDS
 
 END
